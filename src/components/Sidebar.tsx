e1e275cbbd61702c9d3a6a2d9dc149cd712057e3
FranciscoCorreia28 <francisco.correia1804@gmail.com>
2022-07-22 03:23:16 +0100

    fix: logout icon solved at collapsed sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -25,7 +25,7 @@ const cleaLocation = () =>{
     setToogleSidebarCollapse(!toogleSidebarCollapse)
   }
   const collapseSidebarClass = toogleSidebarCollapse ? `mt-2 text-lg font-medium text-blue-200 ` : `mt-2 text-lg font-medium text-blue-200 hidden `;
-  const wrapperSidebarClass = toogleSidebarCollapse ? `h-screen px-4  pt-8 pb-4 bg-white flex flex-col justify-between border  w-80` : `h-screen px-4  pt-8 pb-4 bg-white flex flex-col border border-dashed w-20`;
+  const wrapperSidebarClass = toogleSidebarCollapse ? `h-screen px-4  pt-8 pb-4 bg-white flex flex-col justify-between border  w-80` : `h-screen px-4  pt-8 pb-4 bg-white flex flex-col justify-between border border-dashed w-20`;
   const router = window.location;
   const activeMenu = useMemo(() => menuItems.find(menu => menu.link === router.pathname), [router.pathname])
   const getNavItemClasses = (menu: menuItems | {}) => {
